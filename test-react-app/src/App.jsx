0fd055d5fe794f93ed789e29d8f51aee6c8f6dab
joshgore7 <josh.gore77@gmail.com>
2025-02-24 14:29:07 -0500

    first commit
    Co-authored-by: leviballew <leviballew@users.noreply.github.com>

---
+++ test-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./Card"
+import Details from "./Details"
+
+
+function App() {
+    return (
+        <div className="app">
+            <Card/>
+            <Details/>
+        </div>
+    )
+
+}
+
+export default App
---
+++ test-react-app/src/Card.jsx
@@ -0,0 +1,35 @@
+
+import React, { useState, useEffect } from 'react';
+
+function Card() {
+    const [pokemonData, setPokemonData] = useState([]);
+  
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            const allPokemon = [];
+            for (let i = 1; i <= 151; i++) {
+                const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
+                const data = await res.json();
+                allPokemon.push(data);
+            }
+            setPokemonData(allPokemon);
+        };
+      
+        fetchPokemon();
+    }, []);
+  
+    return (
+        <div className="cards-container">
+            {pokemonData.map((pokemon) => (
+                <div className="card" key={pokemon.id}>
+                    <a href="https://pokeapi.co/">
+                        <img className="card-image" src={pokemon.sprites.front_shiny}/>
+                    </a>
+                    <p>{pokemon.name}</p>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+export default Card;
